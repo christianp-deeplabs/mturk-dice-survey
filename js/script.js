0b15f97c34ee7f1b16f1577c7272ea0d4380dbe5
christianp-deeplabs <84146151+christianp-deeplabs@users.noreply.github.com>
2021-09-15 16:43:16 -0400

    Add initial dice survey files

---
+++ js/script.js
@@ -0,0 +1,114 @@
+$(document).ready(function() {
+
+  var current_fs, next_fs, previous_fs; //fieldsets
+  var opacity;
+  var current = 1;
+  var steps = $("fieldset").length;
+
+  setProgressBar(current);
+
+  $(".next").click(function() {
+
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+
+    //Add Class Active
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({
+      opacity: 0
+    }, {
+      step: function(now) {
+        // for making fielset appear animation
+        opacity = 1 - now;
+
+        current_fs.css({
+          'display': 'none',
+          'position': 'relative'
+        });
+        next_fs.css({
+          'opacity': opacity
+        });
+      },
+      duration: 500
+    });
+    setProgressBar(++current);
+  });
+
+  $(".previous").click(function() {
+
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+
+    //Remove class active
+    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+    //show the previous fieldset
+    previous_fs.show();
+
+    //hide the current fieldset with style
+    current_fs.animate({
+      opacity: 0
+    }, {
+      step: function(now) {
+        // for making fielset appear animation
+        opacity = 1 - now;
+
+        current_fs.css({
+          'display': 'none',
+          'position': 'relative'
+        });
+        previous_fs.css({
+          'opacity': opacity
+        });
+      },
+      duration: 500
+    });
+    setProgressBar(--current);
+  });
+
+  function setProgressBar(curStep) {
+    var percent = parseFloat(100 / steps) * curStep;
+    percent = percent.toFixed();
+    $(".progress-bar")
+      .css("width", percent + "%")
+  }
+
+  $(".roll").click(function() {
+
+    var n = Math.random();
+    n = n * 6;
+    n = Math.floor(n) + 1;
+    //alert('You rolled a ' + n)
+
+    var randomDiceImage = "dice" + n + ".png"; //dice1.png - dice6.png
+
+    var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+    var image1 = document.querySelectorAll("img")[0];
+
+    image1.setAttribute("src", randomImageSource);
+
+    this.disabled = true;
+
+  });
+
+  $('.dropdown-toggle').dropdown()
+
+//  function getDiceNumber() {
+
+//    var n = Math.random();
+//    n = n * 6;
+//    n = Math.floor(n) + 1;
+//    alert('You rolled a ' + n)
+
+//  }
+
+  $(".submit").click(function() {
+    return false;
+  })
+
+});
